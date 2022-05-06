4121154309589c4562f4792fb6fd85df3432e4e5
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-05-06 19:24:25 +0900

    chore: update usage code

--- packages/usage/index.ts
+++ packages/usage/index.ts
@@ -4,41 +4,46 @@ import { createClient, sb } from '@sb-prisma/client'
 import './sb-prisma'
 
 const prisma = createClient<PrismaClient>(PrismaClient)
-const db = new PrismaClient()
+// const db = new PrismaClient()
 
 const main = async () => {
+  await prepare()
+
   const user = await sb(prisma.user.findFirst())
+  console.log('===== prisma.user.findFirst() =====')
   console.dir(user)
 
-  const users = await sb(
-    prisma.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        Team: {
-          select: {
-            name: true,
+  console.log(
+    '===== prisma.user.findMany() with OR condition and ordered desc =====',
+  )
+  console.dir(
+    await sb(
+      prisma.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          Team: {
+            select: {
+              name: true,
+            },
           },
         },
-      },
-      where: {
-        OR: [
-          {
-            name: { contains: 'oo', mode: 'insensitive' },
-          },
-          {
-            name: { contains: 'ar', mode: 'insensitive' },
-          },
-          {
-            name: { contains: 'az', mode: 'insensitive' },
-          },
-        ],
-      },
-      orderBy: { id: 'desc' },
-    }),
+        where: {
+          OR: [
+            {
+              name: { contains: 'oo', mode: 'insensitive' },
+            },
+            {
+              name: { contains: 'az', mode: 'insensitive' },
+            },
+          ],
+        },
+        orderBy: { id: 'desc' },
+      }),
+    ),
   )
-  console.dir(users, { depth: 5 })
 
+  console.log('===== prisma.team.findMany() with double nested model =====')
   console.dir(
     await sb(
       prisma.team.findMany({
@@ -57,23 +62,23 @@ const main = async () => {
           },
         },
         where: {
-          name: { in: ['team1', 'team2'] },
-          id: { gte: 1 },
+          name: { in: ['team2', 'team3'] },
         },
       }),
     ),
-    { depth: 5 },
+    { depth: 4 },
   )
 
+  console.log('===== prisma.user.create() =====')
   console.dir(
     await sb(
       prisma.user.create({
-        data: { name: 'ega', email: 'ewrbrwa@grag.com' },
+        data: { name: 'ega', email: 'ega@example.com' },
       }),
     ),
-    { depth: 5 },
   )
 
+  console.log('===== prisma.user.createMany() with skipDuplicates =====')
   user &&
     console.dir(
       await sb(
@@ -82,9 +87,9 @@ const main = async () => {
           skipDuplicates: true,
         }),
       ),
-      { depth: 5 },
     )
 
+  console.log('===== prisma.user.update() =====')
   user &&
     console.dir(
       await sb(
@@ -93,29 +98,57 @@ const main = async () => {
           where: { id: user.id },
         }),
       ),
-      { depth: 5 },
     )
 
+  console.log('===== prisma.user.updateMany() =====')
   console.dir(
     await sb(
       prisma.user.updateMany({
-        data: { name: 'hoo' },
+        data: { name: 'hee' },
       }),
     ),
-    { depth: 5 },
   )
 
-  user &&
-    console.dir(await sb(prisma.user.delete({ where: { id: user.id } })), {
-      depth: 5,
-    })
+  console.log('===== prisma.user.delete() =====')
+  user && console.dir(await sb(prisma.user.delete({ where: { id: user.id } })))
 
-  const deletedUsers = await sb(
-    prisma.team.deleteMany({
-      where: { name: { contains: 'team' } },
-    }),
+  console.log('===== prisma.team.deleteMany() =====')
+  console.dir(
+    await sb(
+      prisma.team.deleteMany({
+        where: { name: { contains: 'team' } },
+      }),
+    ),
   )
-  console.dir(deletedUsers, { depth: 5 })
 }
 
-main().catch(console.log)
+const prepare = async () => {
+  await cleanUp()
+  await sb(
+    prisma.team.createMany({
+      data: [{ name: 'team1' }, { name: 'team2' }, { name: 'team3' }],
+    }),
+  )
+  const teams = await sb(prisma.team.findMany())
+  await sb(
+    prisma.user.createMany({
+      data: [
+        { name: 'Foo', email: 'foo@example.com', teamId: teams[0].id },
+        { name: 'Bar', email: 'bar@example.com', teamId: teams[0].id },
+        { name: 'Baz', email: 'baz@example.com', teamId: teams[1].id },
+      ],
+    }),
+  )
+}
+
+const cleanUp = async () => {
+  await Promise.all([
+    sb(prisma.user.deleteMany()),
+    sb(prisma.team.deleteMany()),
+  ])
+  console.log('complete cleanup 🧹')
+}
+
+main()
+  .catch(console.log)
+  .finally(() => cleanUp())
