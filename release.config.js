c20d3c6ff66e661d07c3b1d42cee6fdd7f7eb054
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-05-07 00:26:05 +0900

    chore: update release workflow

--- release.config.js
+++ release.config.js
@@ -3,18 +3,12 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
     '@semantic-release/npm',
     '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json'],
+        assets: ['package.json'],
         message:
           'chore(release): set `package.json` to ${nextRelease.version} [skip ci]',
       },
