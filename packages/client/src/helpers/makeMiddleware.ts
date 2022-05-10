8d4f0b722f5f4dc6d20f6fef2ee37a38dc062257
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-05-10 23:33:42 +0900

    fix(client): fix build error

--- packages/client/src/helpers/makeMiddleware.ts
+++ packages/client/src/helpers/makeMiddleware.ts
@@ -1,9 +1,11 @@
 import { RequestBuilder } from '../libs/RequestBuilder'
+// @ts-ignore
 import { Prisma } from '@prisma/client'
 
 export const makeMiddleware = (endpoint: string, apikey: string) => {
   const builder = new RequestBuilder(Prisma.dmmf.datamodel, endpoint, apikey)
 
+  // @ts-ignore
   const middleware: Prisma.Middleware = async ({ args, action, model }) => {
     return await builder.execute(args ?? {}, model ?? '', action)
   }
